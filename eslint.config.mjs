267f7961f02c4f514f850ec305d3b9a327fe89fd
drummpy <154277179+drummpy@users.noreply.github.com>
2025-05-21 10:13:32 -0700

    Update initial project files for dev dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: globals.browser } },
-  pluginVue.configs["flat/essential"],
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
