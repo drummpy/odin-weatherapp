2b62ea0f22fa37f940f5e6c515d22e96ddd610dd
drummpy <154277179+drummpy@users.noreply.github.com>
2025-05-25 12:09:53 -0700

    Change API request to metric units

--- src/weatherapi.js
+++ src/weatherapi.js
@@ -1,5 +1,5 @@
 export async function getWeather(location) {
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=65SXAJHLF8RPX8AYSA438AZG7`;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=65SXAJHLF8RPX8AYSA438AZG7&unitGroup=metric`;
   try {
     const response = await fetch(url, { mode: "cors" });
     const data = await response.json();
